d7df45a6007fb83b41641e6d4d47a4882edf0d9b
pebabion <kelvin@abillion.com>
2021-12-02 22:31:17 +0800

    Change DAG name

--- another_dag.py
+++ another_dag.py
@@ -3,13 +3,13 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
-def print_hello():
-    return 'Hello world from first Airflow DAG!'
+def print_message():
+    return 'Just another DAG!'
 
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('another_dag', description='Another DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+another_operator = PythonOperator(task_id='another_dag', python_callable=print_message, dag=dag)
 
-hello_operator
+another_operator
